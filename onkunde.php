d4dd397c83fd5ce0a18c0f07a95a1d4122eeec10
Dennis van Blanken <99040363@mydavinci.nl>
2017-03-15 11:03:01 +0100

    Done

---
+++ onkunde.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Onkunde</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div id="outer">
+<div id="title">
+<h2>Mad Libs</h2>
+</div>
+<div id="bar"> <a href="index.php">Er heerst paniek...</a> <a href="onkunde.php"> Onkunde </a> </div>
+
+
+<h1>Onkunde</h1>
+
+<form action="welcome2.php" method="post">
+<div id="container">
+<p> Wat zou je graag willen kunnen? <input type="text" name="kunnen"> </p>
+
+<p> Met welke persoon kun je goed opschieten? <input type="text" name="persoon"> </p>
+
+<p> Wat is je favoriete getal? <input type="text" name="getal"> </p>
+
+<p> Wat heb je altijd bij je als op vakantie gaat? <input type="text" name="voorwerp"> </p>
+
+<p> Wat is je beste persoonlijke eigenschap? <input type="text" name="beste"> </p>
+
+<p> Wat is je slechtste persoonlijke eigenschap? <input type="text" name="slechtste"> </p>
+
+<p> Wat is het ergste dat je kan overkomen? <input type="text" name="ergste"> </p>
+
+</div>
+
+<input type="submit" value="Versturen">
+
+</form>
+</div>
+<footer><p id="footer">Made by Dennis</p></footer>
+</body>
+</html>
